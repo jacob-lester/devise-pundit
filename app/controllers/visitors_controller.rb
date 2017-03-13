f498c19a08c9078c9996c7aa0aa3d020644a973b
Jacob Lester <lester1423@gmail.com>
2017-03-13 04:26:59 +0000

    Single question page template

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,6 @@ class VisitorsController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @questions = Question.all
+    @questions = Question.order('created_at DESC')
   end
 end
