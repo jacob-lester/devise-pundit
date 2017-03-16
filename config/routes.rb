6403998f32a5a127af62ea271fc5056095ff3635
Jacob Lester <lester1423@gmail.com>
2017-03-16 22:47:18 +0000

    admin create user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :answers
   resources :questions
   root to: 'visitors#index'
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
 end
